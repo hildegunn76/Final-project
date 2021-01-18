bc9d4aebd23a2c1deea2005ed3e34c501fe6807d
hildegunn76 <77569969+hildegunn76@users.noreply.github.com>
2021-01-18 21:10:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
+
+<script>
+    {var username = principal;
+var username = rate;
+var username = years;
+var username = interest; value = principal * years * rate / 100
+   
+    }
+  </script>
         
